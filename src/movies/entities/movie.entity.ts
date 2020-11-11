c7e22dbd8eebac0310288bd160b5092ff182a4e4
박준수 <pksjmh5295@gmail.com>
2020-11-11 11:43:42 +0900

    2.2 Movie Service

---
+++ src/movies/entities/movie.entity.ts
@@ -0,0 +1,9 @@
+//In regular case, Entities have DB models
+//However, in my case, using the Javascript Object
+
+export class Movie {
+  id: number;
+  title: string;
+  year: number;
+  genre: string;
+}
--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,41 +6,38 @@ import {
   Param,
   Patch,
   Post,
-  Query,
 } from "@nestjs/common";
+import { MoviesService } from "./movies.service";
+import { Movie } from "./entities/movie.entity";
 
 @Controller("movies")
 export class MoviesController {
+  constructor(private readonly moviesService: MoviesService) {}
+
   @Get()
-  getAll() {
-    return "This will return all movies";
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
   }
 
-  @Get("/search")
-  search(@Query("year") searchingYear) {
-    return `We are searching for a movie made after: ${searchingYear} `;
-  }
-
-  @Get("/:id")
-  getOne(@Param("id") movieId: string) {
-    return `This will return one movie with the id: ${movieId}`;
+  @Get(":id")
+  getOne(@Param("id") movieId: string): Movie {
+    return this.moviesService.getOne(movieId);
   }
 
   @Post()
   create(@Body() movieData) {
-    //console.log(movieData);
-    return movieData;
+    return this.moviesService.create(movieData);
   }
 
-  @Delete("/:id")
+  @Delete(":id")
   remove(@Param("id") movieId: string) {
-    return `This will delete a movie with the id: ${movieId}`;
+    return this.moviesService.deleteOne(movieId);
   }
 
-  @Patch("/:id")
-  path(@Param("id") movieId: string, @Body() updateData) {
+  @Patch(":id")
+  patch(@Param("id") movieId: string, @Body() updateData) {
     return {
-      updateMovie: movieId,
+      updatedMovie: movieId,
       ...updateData,
     };
   }
